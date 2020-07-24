46ea2eae23f7089dde7daba89da9bd1bc10979f2
Seurfo002 <34084859+Seurfo002@users.noreply.github.com>
2020-07-24 22:27:21 +0300

    First commit

---
+++ src/main/java/fr/cubixmc/api/CubixAPI.java
@@ -0,0 +1,16 @@
+package fr.cubixmc.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CubixAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {   
+
+    }
+}
